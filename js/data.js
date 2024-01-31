5e9aea791a8f546764fa50f2450bdcd50aabbd79
alvesmarcola <alvesbrunom8@gmail.com>
2024-01-31 13:11:14 -0300

    Primeira pagina - Finalizada!

---
+++ js/data.js
@@ -0,0 +1,30 @@
+const movies = [
+    {
+        img: 'https://img.youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg',
+        title: 'Growcast #00 Conheça a Growdev',
+        link: 'https://youtu.be/aJ-HZuLcKeA?si=eX0XYg9h0dPrdvfQ',
+        category: 'growcast', // aqui se faz a separação das categorias, uma por categoria.
+    },
+    {
+        img: '',
+        title: '',
+        link: '',
+        category: 'webinar', 
+    },
+    {
+        img: '',
+        title: '',
+        link: '',
+        category: 'ux-ui', 
+    },
+    {
+        img: '',
+        title: '',
+        link: '',
+        category: 'other', 
+    }
+]
+
+const growcast = movies.filter( ()=>{
+
+})
